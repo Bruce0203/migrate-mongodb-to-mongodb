b136cb3d5c31b1463f87e319dd0b798483689e62
Bruce0203 <BruceFreedy@gmail.com>
2022-11-06 01:42:32 +0900

    fix: Main-Class attributes

--- src/main/kotlin/MigrateMongodbToMongodb.kt
+++ src/main/kotlin/MigrateMongodbToMongodb.kt
@@ -10,26 +10,37 @@ fun main() {
 
 fun migrate(oldName: String, newName: String) {
     val errors = ArrayList<String>()
-    var newClient = MongoClients.create(newName)
-    val oldClient = MongoClients.create(oldName)
-    oldClient.listDatabaseNames().forEach { dbName ->
-        val newDB = newClient.getDatabase(dbName)
-        val oldDB = oldClient.getDatabase(dbName)
-        oldDB.listCollectionNames().forEach { colName ->
-            newDB.createCollection(colName)
-            val newCol = newDB.getCollection(colName)
-            val oldCol = oldDB.getCollection(colName)
-            oldCol.find().forEach {
-                println(it)
-                try {
-                    newCol.insertOne(it)
-                } catch(e: Exception) {
-                    newClient = MongoClients.create(newName)
-                    e.stackTraceToString().apply { errors.add(it.toString() + this) }
+
+
+    loop@while (true) {
+        try {
+            val newClient = MongoClients.create(newName)
+            val oldClient = MongoClients.create(oldName)
+            oldClient.listDatabaseNames().forEach { dbName ->
+                val newDB = newClient.getDatabase(dbName)
+                val oldDB = oldClient.getDatabase(dbName)
+                oldDB.listCollectionNames().forEach { colName ->
+                    newDB.createCollection(colName)
+                    val newCol = newDB.getCollection(colName)
+                    val oldCol = oldDB.getCollection(colName)
+                    oldCol.find().forEach {
+                        println(it)
+                        try {
+                            newCol.insertOne(it)
+                        } catch(e: Exception) {
+                            if (e.stackTraceToString().contains("cannot create a non-capped oplog collection")) {
+                                e.stackTraceToString().apply { errors.add(it.toString() + this) }
+                                throw AssertionError()
+                            }
+
+                        }
+                    }
                 }
             }
+            errors.forEach(::println)
+            println("Done! ${errors.size} errors skipped!")
+        } catch (_: Exception) {
+
         }
     }
-    errors.forEach(::println)
-    println("Done! ${errors.size} errors skipped!")
 }
