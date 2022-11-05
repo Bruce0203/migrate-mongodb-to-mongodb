2c48282434dc5bd10f86c4e0055d0a1264b4dae3
Bruce0203 <BruceFreedy@gmail.com>
2022-11-06 01:53:13 +0900

    fix: Main-Class attributes

--- src/main/kotlin/MigrateMongodbToMongodb.kt
+++ src/main/kotlin/MigrateMongodbToMongodb.kt
@@ -8,27 +8,41 @@ fun main() {
     migrate(prop.getProperty("old")!!, prop.getProperty("new")!!)
 }
 
+const val refreshPeriod = 20000
 fun migrate(oldName: String, newName: String) {
     val errors = ArrayList<String>()
     val newClient = MongoClients.create(newName)
     val oldClient = MongoClients.create(oldName)
-    oldClient.listDatabaseNames().forEach { dbName ->
-        val newDB = newClient.getDatabase(dbName)
-        val oldDB = oldClient.getDatabase(dbName)
-        oldDB.listCollectionNames().forEach { colName ->
-            newDB.createCollection(colName)
-            val newCol = newDB.getCollection(colName)
-            val oldCol = oldDB.getCollection(colName)
-            oldCol.find().forEach {
-                println(it)
-                try {
-                    newCol.insertOne(it)
-                } catch(e: Exception) {
-                    e.stackTraceToString().apply { errors.add(it.toString() + this) }
+    var i = 0
+    var lastI = refreshPeriod
+    while(true) {
+        try {
+            oldClient.listDatabaseNames().forEach { dbName ->
+                val newDB = newClient.getDatabase(dbName)
+                val oldDB = oldClient.getDatabase(dbName)
+                oldDB.listCollectionNames().forEach { colName ->
+                    newDB.createCollection(colName)
+                    val newCol = newDB.getCollection(colName)
+                    val oldCol = oldDB.getCollection(colName)
+                    oldCol.find().forEach {
+                        println(it)
+                        try {
+                            i++
+                            if (i <= lastI) {
+                                return@forEach
+                            } else if (i >= refreshPeriod) {
+                                lastI += i
+                                i = 0
+                            }
+                            newCol.insertOne(it)
+                        } catch(e: Exception) {
+                            e.stackTraceToString().apply { errors.add(it.toString() + this) }
+                        }
+                    }
                 }
             }
-        }
+            errors.forEach(::println)
+            println("Done! ${errors.size} errors skipped!")
+        } catch(_: Exception) {}
     }
-    errors.forEach(::println)
-    println("Done! ${errors.size} errors skipped!")
 }
