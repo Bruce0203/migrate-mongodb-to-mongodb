c037a4bf962888dbe85647b2e6a6e17c6d18ae11
Bruce0203 <BruceFreedy@gmail.com>
2022-11-13 08:27:06 +0900

    fix: Main-Class attributes

--- src/main/kotlin/MigrateMongodbToMongodb.kt
+++ src/main/kotlin/MigrateMongodbToMongodb.kt
@@ -1,4 +1,5 @@
 import com.mongodb.client.MongoClients
+import org.bson.Document
 import java.io.File
 import java.util.Properties
 
@@ -10,8 +11,9 @@ fun main() {
 
 fun migrate(oldName: String, newName: String) {
     val errors = ArrayList<String>()
-    val newClient = MongoClients.create(newName)
-    val oldClient = MongoClients.create(oldName)
+    val queue = ArrayList<Pair<Pair<String, String>, Document>>()
+    var newClient = MongoClients.create(newName)
+    var oldClient = MongoClients.create(oldName)
     oldClient.listDatabaseNames().forEach { dbName ->
         val oldDB = oldClient.getDatabase(dbName)
         oldDB.listCollectionNames().forEach { colName ->
@@ -22,11 +24,17 @@ fun migrate(oldName: String, newName: String) {
                     println(it)
                     newCol.insertOne(it)
                 } catch(e: Exception) {
+                    queue.add(Pair(Pair(dbName, colName), it))
                     e.stackTraceToString().apply { errors.add(it.toString() + this) }
                 }
             }
         }
     }
     errors.forEach(::println)
+    newClient = MongoClients.create(newName)
+    oldClient = MongoClients.create(oldName)
+    queue.forEach {
+        newClient.getDatabase(it.first.first).getCollection(it.first.second).insertOne(it.second)
+    }
     println("Done! ${errors.size} errors skipped!")
 }
