d2c35539af7eea68f3b0ceba6d3f29fbdd5195e4
Bruce0203 <BruceFreedy@gmail.com>
2022-11-12 16:35:26 +0900

    fix: Main-Class attributes

--- src/main/kotlin/MigrateMongodbToMongodb.kt
+++ src/main/kotlin/MigrateMongodbToMongodb.kt
@@ -12,27 +12,21 @@ fun migrate(oldName: String, newName: String) {
     val errors = ArrayList<String>()
     val newClient = MongoClients.create(newName)
     val oldClient = MongoClients.create(oldName)
-    while(true) {
-        try {
-            oldClient.listDatabaseNames().forEach { dbName ->
-                val newDB = newClient.getDatabase(dbName)
-                val oldDB = oldClient.getDatabase(dbName)
-                oldDB.listCollectionNames().forEach { colName ->
-                    newDB.createCollection(colName)
-                    val newCol = newClient.getDatabase(dbName).getCollection(colName)
-                    val oldCol = oldDB.getCollection(colName)
-                    oldCol.find().forEach {
-                        try {
-                            println(it)
-                            newCol.insertOne(it)
-                        } catch(e: Exception) {
-                            e.stackTraceToString().apply { errors.add(it.toString() + this) }
-                        }
-                    }
+    oldClient.listDatabaseNames().forEach { dbName ->
+        val oldDB = oldClient.getDatabase(dbName)
+        oldDB.listCollectionNames().forEach { colName ->
+            val newCol = newClient.getDatabase(dbName).getCollection(colName)
+            val oldCol = oldDB.getCollection(colName)
+            oldCol.find().forEach {
+                try {
+                    println(it)
+                    newCol.insertOne(it)
+                } catch(e: Exception) {
+                    e.stackTraceToString().apply { errors.add(it.toString() + this) }
                 }
             }
-            errors.forEach(::println)
-            println("Done! ${errors.size} errors skipped!")
-        } catch(_: Exception) {}
+        }
     }
+    errors.forEach(::println)
+    println("Done! ${errors.size} errors skipped!")
 }
